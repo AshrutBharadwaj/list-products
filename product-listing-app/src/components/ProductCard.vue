89920efe8a2fd7022d4f751d49276e60188bb487
Ashrut Bharadwaj <ashrutbharadwaj@Ashruts-MacBook-Pro.local>
2025-04-08 22:17:51 +0530

    FIXING #Minor change

--- product-listing-app/src/components/ProductCard.vue
+++ product-listing-app/src/components/ProductCard.vue
@@ -3,7 +3,7 @@
     <img :src="product.images[0]" :alt="product.title" class="card-image" />
     <div class="card-body">
       <h2 class="card-title">{{ product.title }}</h2>
-      <p class="card-price">\${{ product.price.toFixed(2) }}</p>
+      <p class="card-price">${{ product.price.toFixed(2) }}</p>
       <p class="card-stock" :class="product.availabilityStatus === 'Out of Stock' ? 'out-of-stock' : product.availabilityStatus === 'Low Stock' ? 'low-in-stock' : 'card-stock'">
         {{ product.availabilityStatus }}
       </p>
