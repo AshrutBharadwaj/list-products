0db475aa24b34d8ca72d8e1b4278590aac8aaf79
Ashrut Bharadwaj <ashrutbharadwaj@Ashruts-MacBook-Pro.local>
2025-04-09 14:25:54 +0530

    FIXING #Some css

--- product-listing-app/src/components/ProductCard.vue
+++ product-listing-app/src/components/ProductCard.vue
@@ -1,29 +1,31 @@
 <template>
-  <div class="product-card">
-    <div class="image-wrapper">
-      <img
-        :src="product.thumbnail"
-        :alt="product.title"
-        loading="lazy"
-        @load="loaded = true"
-        :class="{ loaded }"
-      />
+  <transition name="fade-scale">
+    <div class="product-card">
+      <div class="image-wrapper">
+        <img
+          :src="product.thumbnail"
+          :alt="product.title"
+          loading="lazy"
+          @load="loaded = true"
+          :class="{ loaded }"
+        />
+      </div>
+      <h3 class="title">{{ product.title }}</h3>
+      <p class="price">${{ product.price }}</p>
+      <p
+        class="card-stock"
+        :class="
+          product.availabilityStatus === 'Out of Stock'
+            ? 'out-of-stock'
+            : product.availabilityStatus === 'Low Stock'
+            ? 'low-in-stock'
+            : 'card-stock'
+        "
+      >
+        {{ product.availabilityStatus }}
+      </p>
     </div>
-    <h3 class="title">{{ product.title }}</h3>
-    <p class="price">${{ product.price }}</p>
-    <p
-      class="card-stock"
-      :class="
-        product.availabilityStatus === 'Out of Stock'
-          ? 'out-of-stock'
-          : product.availabilityStatus === 'Low Stock'
-          ? 'low-in-stock'
-          : 'card-stock'
-      "
-    >
-      {{ product.availabilityStatus }}
-    </p>
-  </div>
+  </transition>
 </template>
 
 <script setup>
