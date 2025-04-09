e0d6d81f062b6f213e493bf8d58c8f9b4a0ae75c
Ashrut Bharadwaj <ashrutbharadwaj@Ashruts-MacBook-Pro.local>
2025-04-09 18:05:04 +0530

    FIXING #Major issue with loading

--- product-listing-app/src/components/ProductCard.vue
+++ product-listing-app/src/components/ProductCard.vue
@@ -1,31 +1,29 @@
 <template>
-  <transition name="fade-scale">
-    <div class="product-card" v-show="loaded">
-      <div class="image-wrapper">
-        <img
-          :src="product.thumbnail"
-          :alt="product.title"
-          loading="lazy"
-          @load="loaded = true"
-          :class="{ loaded }"
-        />
-      </div>
-      <h3 class="title">{{ product.title }}</h3>
-      <p class="price">${{ product.price }}</p>
-      <p
-        class="card-stock"
-        :class="
-          product.availabilityStatus === 'Out of Stock'
-            ? 'out-of-stock'
-            : product.availabilityStatus === 'Low Stock'
-            ? 'low-in-stock'
-            : 'card-stock'
-        "
-      >
-        {{ product.availabilityStatus }}
-      </p>
+  <div class="product-card">
+    <div class="image-wrapper">
+      <img
+        :src="product.thumbnail"
+        :alt="product.title"
+        loading="lazy"
+        @load="loaded = true"
+        :class="{ loaded }"
+      />
     </div>
-  </transition>
+    <h3 class="title">{{ product.title }}</h3>
+    <p class="price">${{ product.price }}</p>
+    <p
+      class="card-stock"
+      :class="
+        product.availabilityStatus === 'Out of Stock'
+          ? 'out-of-stock'
+          : product.availabilityStatus === 'Low Stock'
+          ? 'low-in-stock'
+          : 'card-stock'
+      "
+    >
+      {{ product.availabilityStatus }}
+    </p>
+  </div>
 </template>
 
 <script setup>
